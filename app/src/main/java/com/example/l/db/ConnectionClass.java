7c7c3b0643d6f35d8a48473fe6328348ae0a8783
ibrahimRD <brh0o0m10@hotmail.com>
2016-03-03 17:10:08 +0300

    Initial commit

---
+++ app/src/main/java/com/example/l/db/ConnectionClass.java
@@ -0,0 +1,42 @@
+package com.example.l.db;
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.util.Log;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+
+/**
+ * Created by h-pc on 16-Oct-15.
+ */
+public class ConnectionClass {
+    String ip = "10.131.198.144";
+    String classs = "com.microsoft.sqlserver.jdbc.SqlServerDriver";
+    String db = "Andro";
+    String un = "L";
+    String password = "";
+    @SuppressLint("NewApi")
+    public Connection CONN() {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection conn = null;
+        String ConnURL = null;
+        try {
+            Class.forName(classs);
+            ConnURL = "jdbc:jtds:sqlserver://" + ip + ";"
+                    + "databaseName=" + db + ";user=" + un + ";password="
+                    + password + ";";
+            conn = DriverManager.getConnection("jdbc:odbc:Andro_DSN");
+        } catch (SQLException se) {
+            Log.e("ERRO", se.getMessage());
+        } catch (ClassNotFoundException e) {
+            Log.e("ERRO", e.getMessage());
+        } catch (Exception e) {
+            Log.e("ERRO", e.getMessage());
+        }
+        return conn;
+    }
+}
